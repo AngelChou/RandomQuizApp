4455321d96c2fc347ccc14094424d663c3bf80f9
Angel Chou <cat77120377@hotmail.com>
2018-10-19 14:06:19 +0800

    Add 10 country flags and design storyboard, connect unit with IBOutlet and IBAction

--- RandomQuizApp/ViewController.swift
+++ RandomQuizApp/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionNumLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var flagImage: UIImageView!
+    @IBAction func showAnswerButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextQuestionButton(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
